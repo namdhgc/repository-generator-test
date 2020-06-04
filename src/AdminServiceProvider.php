3eeb1873e93248b921c1574f6b225dbdef5fa230
Hoai Nam <namdhgc@gmail.com>
2020-06-04 18:25:41 +0700

    update namespace

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Namdhgc\Admin\RepositoryGenerator;
 
 use Illuminate\Support\ServiceProvider;
-use Namdhgc\Admin\Commands\RepositoryGenerator;
 
 class AdminServiceProvider extends ServiceProvider
 {
@@ -28,11 +27,5 @@ class AdminServiceProvider extends ServiceProvider
             __DIR__.'/commands' => base_path('app/Console/Commands/'),
             __DIR__.'/stubs' => base_path('resources/stubs/'),
         ], "repositiry-generator");
-
-        // if ($this->app->runningInConsole()) {
-        //     $this->commands([
-        //         RepositoryGenerator::class,
-        //      ]);
-        // }
     }
 }
--- src/commands/RepositoryGenerator.php
+++ src/commands/RepositoryGenerator.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Namdhgc\Admin\Commands;
+namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
